033ee37e3c4183d7810e01f30ef3bc5aff997d55
Julien Desfossez <jdesfossez@efficios.com>
2014-07-18 17:18:10 -0400

    script to deploy the analysis scripts
    Signed-off-by: Julien Desfossez <jdesfossez@efficios.com>

---
+++ scripts/deploy-analysis.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+source /etc/lsb-release
+if [ "$DISTRIB_ID" != "Ubuntu" ]; then
+	echo "ERROR: Only Ubuntu is supported."
+	exit 1
+fi
+
+if test $# -lt 1; then
+	echo "Usage : $0 <tracevisor hostname/ip>"
+	exit 1
+fi
+
+TRACEVISORIP=$1
+TRACEVISORPORT=5000
+
+apt-get update
+apt-get install -y software-properties-common curl git python3-progressbar python3-pymongo
+
+apt-add-repository -y ppa:lttng/ppa
+
+apt-get update
+apt-get -y install babeltrace python3-babeltrace
+
+cd /usr/local/src/
+git clone https://github.com/jdesfossez/lttng-analyses.git
+
+mkdir -p ~/.ssh
+curl -s http://${TRACEVISORIP}:${TRACEVISORPORT}/trace/api/v1.0/ssh|grep tracevisor | cut -d '"' -f2 | cut -d "\\" -f1 >> ~/.ssh/authorized_keys2
+
+# check if we have to use the gateway ("via" keyword)
+#ip -4 route get ${TRACEVISORIP} 2>/dev/null | grep via >/dev/null
+#if test $? = 0; then
+#	ipv4=$(ip -4 route get ${TRACEVISORIP} 2>/dev/null | grep dev | awk '{print $7}')
+#else
+#	ipv4=$(ip -4 route get ${TRACEVISORIP} 2>/dev/null | grep dev | awk '{print $5}')
+#fi
+
+# check if we have to use the gateway ("via" keyword)
+#ip -6 route get ${TRACEVISORIP} 2>/dev/null | grep via >/dev/null
+#if test $? = 0; then
+#	ipv6=$(ip -6 route get ${TRACEVISORIP} 2>/dev/null | grep dev | awk '{print $7}')
+#else
+#	ipv6=$(ip -6 route get ${TRACEVISORIP} 2>/dev/null | grep dev | awk '{print $5}')
+#fi
+
+#curl -i -H "Content-Type: application/json" -X POST -d "{\"hostname\": \"$HOSTNAME\", \"ipv4\":\"$ipv4\", \"ipv6\":\"$ipv6\" }" http://${TRACEVISORIP}:${TRACEVISORPORT}/trace/api/v1.0/add_relay
