90e9545084b6bfef26a2544715a88eb1a03f8035
Julien Desfossez <jdesfossez@efficios.com>
2014-07-18 10:23:12 -0400

    fix tracevisor home path in upstart script
    Signed-off-by: Julien Desfossez <jdesfossez@efficios.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,6 @@ adduser --system tracevisor --home /var/lib/tracevisor
 cd /var/lib/tracevisor
 git clone https://github.com/jdesfossez/tracevisor.git
 cp tracevisor/upstart/tracevisor.conf /etc/init/
-sed -i 's#exec /opt/tracevisor/tracevisor.py#exec sudo -u tracevisor /var/lib/tracevisor/tracevisor/tracevisor.py#' /etc/init/tracevisor.conf
 mkdir /var/lib/tracevisor/.ssh
 ssh-keygen -C tracevisor@tracevisor -t rsa -b 2048 -N "" -f /var/lib/tracevisor/.ssh/id_rsa_tracevisor
 chown -R tracevisor:nogroup /var/lib/tracevisor/
